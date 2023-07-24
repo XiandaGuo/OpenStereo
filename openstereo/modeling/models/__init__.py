2a5f9e968e76c81ff6179e6928f037d54f14bb9f
Juntao <ralph0813.ljt@gmail.com>
2023-07-24 10:57:57 +0800

    igev

--- openstereo/modeling/models/__init__.py
+++ openstereo/modeling/models/__init__.py
@@ -2,9 +2,6 @@
 from .acvnet import ACVNet
 from .casnet import CasStereoNet
 from .cfnet import CFNet
-from .CoEx import CoExNet
-# from .dsmnet import DSMNet1x
-# from .dsmnet import DSMNet2x
 # from .ganet import GANet
 from .coex import CoExNet
 from .fadnet import FADNet
@@ -16,8 +13,4 @@ from .msnet import MSNet
 from .psmnet import PSMNet
 from .raftstereo import RAFT_Stereo
 from .sttr import STTRNet
-try:
-    from .aanet import AANet
-except ImportError:
-    pass
-    #print('AANet is not available')
+from .igev import IGEV
