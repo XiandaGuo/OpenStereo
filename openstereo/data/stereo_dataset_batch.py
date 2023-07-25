d59feb3240efc3e7e2c76752861a531ae15afe70
Juntao <ralph0813.ljt@gmail.com>
2023-07-26 00:21:47 +0800

    igev

--- openstereo/data/stereo_dataset_batch.py
+++ openstereo/data/stereo_dataset_batch.py
@@ -227,10 +227,12 @@ class StereoBatchDataset(Dataset):
                 transform_compose.append(ST.NormalizeImage(trans['mean'], trans['std']))
             elif trans['type'] == 'FlowAugmentor':
                 transform_compose.append(ST.FlowAugmentor(trans['size'], trans['min_scale'], trans['max_scale'], trans['do_flip'], trans['saturation_range']))
+            elif trans['type'] == 'SparseFlowAugmentor':
+                transform_compose.append(ST.SparseFlowAugmentor(trans['size'], trans['min_scale'], trans['max_scale'], trans['do_flip']))
             elif trans['type'] == 'ColorTransform':
-                transform_compose.append(ST.ColorTransform(trans['saturation_range']))
+                transform_compose.append(ST.ColorTransform(trans['range']))
             elif trans['type'] == 'EraserTransform':
-                transform_compose.append(ST.EraserTransform(trans['eraser_aug_prob']))
+                transform_compose.append(ST.EraserTransform(trans['prob']))
             elif trans['type'] == 'SpatialTransform':
                 transform_compose.append(ST.SpatialTransform(trans['size'], trans['min_scale'], trans['max_scale'], trans['do_flip']))
             elif trans['type'] == 'RandomFlip':
