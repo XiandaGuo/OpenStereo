8a59449cd647026d52a7997538736e321f7ddaf9
Juntao Lu <ralph0813.ljt@gmail.com>
2023-10-11 22:44:21 +0800

    fix acv

--- openstereo/modeling/models/acvnet/model.py
+++ openstereo/modeling/models/acvnet/model.py
@@ -1,40 +1,54 @@
+import torch
 import torch.nn.functional as F
-import torch 
 
 from modeling.base_model import BaseModel
-from .acvnet import ACV_Net
-from .acvnet_small import ACV_NetSmall
+from .acvnet import ACVNet as Net
+from .acvnet_small import ACVNetSmall as NetSmall
 
 
 class ACVLoss:
-    def __init__(self):
-        self.info = {}
+    def __init__(self, loss_type='attn_only'):
+        super().__init__()
+        assert loss_type in ['attn_only', 'freeze_attn', 'full', 'test'], f"loss_type {loss_type} not supported"
+        self.loss_fn = None
+        if loss_type == 'attn_only':
+            self.loss_fn = self.model_loss_train_attn_only
+        elif loss_type == 'freeze_attn':
+            self.loss_fn = self.model_loss_train_freeze_attn
+        elif loss_type == 'full':
+            self.loss_fn = self.model_loss_train
+        elif loss_type == 'test':
+            self.loss_fn = self.model_loss_test
+        else:
+            raise NotImplementedError(f"loss_type {loss_type} not supported")
 
-    """ Loss function defined over sequence of flow predictions """
-
-    def model_loss_train_attn_only(self, disp_ests, disp_gt, mask):
+    @staticmethod
+    def model_loss_train_attn_only(disp_ests, disp_gt, mask):
         weights = [1.0]
         all_losses = []
         for disp_est, weight in zip(disp_ests, weights):
             all_losses.append(weight * F.smooth_l1_loss(disp_est[mask], disp_gt[mask], size_average=True))
         return sum(all_losses)
 
-    def model_loss_train_freeze_attn(self, disp_ests, disp_gt, mask):
+    @staticmethod
+    def model_loss_train_freeze_attn(disp_ests, disp_gt, mask):
         weights = [0.5, 0.7, 1.0]
         all_losses = []
         for disp_est, weight in zip(disp_ests, weights):
             all_losses.append(weight * F.smooth_l1_loss(disp_est[mask], disp_gt[mask], size_average=True))
         return sum(all_losses)
-        
-    def model_loss_train(self, disp_ests, disp_gt, mask):
-        weights = [0.5, 0.5, 0.7, 1.0] 
+
+    @staticmethod
+    def model_loss_train(disp_ests, disp_gt, mask):
+        weights = [0.5, 0.5, 0.7, 1.0]
         all_losses = []
         for disp_est, weight in zip(disp_ests, weights):
             all_losses.append(weight * F.smooth_l1_loss(disp_est[mask], disp_gt[mask], size_average=True))
         return sum(all_losses)
-        
-    def model_loss_test(self, disp_ests, disp_gt, mask):
-        weights = [1.0] 
+
+    @staticmethod
+    def model_loss_test(disp_ests, disp_gt, mask):
+        weights = [1.0]
         all_losses = []
         for disp_est, weight in zip(disp_ests, weights):
             all_losses.append(weight * F.l1_loss(disp_est[mask], disp_gt[mask], size_average=True))
@@ -44,11 +58,12 @@ class ACVLoss:
         disp_ests = training_output["disp"]["disp_ests"]
         disp_gt = training_output["disp"]["disp_gt"]
         mask = training_output["disp"]["mask"]
-        total_loss = self.model_loss_train(disp_ests, disp_gt, mask)
-
-        self.info['scalar/total_loss'] = total_loss.item()
-
-        return total_loss, self.info
+        assert self.loss_fn is not None, "loss_fn not initialized"
+        total_loss = self.loss_fn(disp_ests, disp_gt, mask)
+        loss_info = {}
+        loss_info['scalar/train/loss_disp'] = total_loss
+        loss_info['scalar/train/loss_sum'] = total_loss
+        return total_loss, loss_info
 
 
 class ACVNet(BaseModel):
@@ -56,60 +71,29 @@ class ACVNet(BaseModel):
         super().__init__(*args, **kwargs)
 
     def build_network(self):
-        model_cfg = self.model_cfg
-        maxdisp = model_cfg['base_config']['max_disp']
-        attn_weights_only = model_cfg['base_config']['attn_weights_only']
-        freeze_attn_weights = model_cfg['base_config']['freeze_attn_weights']
-        self.net = ACV_Net(maxdisp, attn_weights_only, freeze_attn_weights)
-        # self.net.freeze_bn() # legacy, could be removed
-    
-    """
-    def get_loss_func(self, loss_cfg):
-        self.loss_fn = ACVLoss()
-    """
-    
+        attn_weights_only = self.model_cfg['base_config']['attn_weights_only']
+        freeze_attn_weights = self.model_cfg['base_config']['freeze_attn_weights']
+        if self.model_cfg.get('small_model', False):
+            self.net = NetSmall(self.max_disp, attn_weights_only, freeze_attn_weights)
+        else:
+            self.net = Net(self.max_disp, attn_weights_only, freeze_attn_weights)
+
     def build_loss_fn(self):
-        loss_cfg = self.cfg['loss_cfg']
-        self.loss_fn = ACVLoss()
-
-    def prepare_inputs(self, inputs, device=None, **kwargs):
-        """Reorganize input data for different models
-
-        Args:
-            inputs: the input data.
-            device: the device to put the data.
-        Returns:
-            dict: training data including ref_img, tgt_img, disp image,
-                  and other meta data.
-        """
-        processed_inputs = {
-            'left': inputs['left'],
-            'right': inputs['right'],
-        }
-        if 'disp' in inputs.keys():
-            disp_gt = inputs['disp']
-            # compute the mask of valid disp_gt
-            mask = (disp_gt < self.max_disp) & (disp_gt > 0)
-            processed_inputs.update({
-                'disp_gt': disp_gt,
-                'mask': mask,
-            })
-        if not self.training:
-            for k in ['pad', 'name']:
-                if k in inputs.keys():
-                    processed_inputs[k] = inputs[k]
-        if device is not None:
-            # move data to device
-            for k, v in processed_inputs.items():
-                processed_inputs[k] = v.to(device) if torch.is_tensor(v) else v
-        return processed_inputs
+        if self.model_cfg['base_config']['attn_weights_only']:
+            loss_type = 'attn_only'
+        elif self.model_cfg['base_config']['freeze_attn_weights']:
+            loss_type = 'freeze_attn'
+        else:
+            loss_type = 'full'
+        self.loss_fn = ACVLoss(loss_type)
 
     def forward(self, inputs):
         """Forward the network."""
-        left = inputs["left"]
-        right = inputs["right"]
+        ref_img = inputs["ref_img"]
+        tgt_img = inputs["tgt_img"]
         if self.training:
-            sequence_output = self.net(left, right, training=True)
+            sequence_output = self.net(ref_img, tgt_img)
+            res = sequence_output[-1]
             output = {
                 "training_disp": {
                     "disp": {
@@ -118,15 +102,30 @@ class ACVNet(BaseModel):
                         "mask": inputs['mask']
                     },
                 },
-                "visual_summary": {},
+                "visual_summary": {
+                    "image/train/image_c": torch.cat([ref_img[0], tgt_img[0]], dim=1),
+                    "image/train/disp_c": torch.cat(
+                        [inputs["disp_gt"][0], res[0]],
+                        dim=0
+                    )
+                }
             }
         else:
-            pred2 = self.net(left, right, training=False)
-            # print(pred2[0].shape)
+            pred2 = self.net(ref_img, tgt_img)
+            res = pred2[-1]
             output = {
                 "inference_disp": {
-                    "disp_est": pred2[0]
+                    "disp_est": res
                 },
-                "visual_summary": {}
+                "visual_summary": {
+                    "image/test/image_c": torch.cat([ref_img[0], tgt_img[0]], dim=1),
+                    "image/test/disp_c": res[0]
+                }
             }
+            if 'disp_gt' in inputs:
+                disp_gt = inputs['disp_gt']
+                output['visual_summary'] = {
+                    "image/val/image_c": torch.cat([ref_img[0], tgt_img[0]], dim=1),
+                    "image/val/disp_c": torch.cat([disp_gt[0], res[0]], dim=0)
+                }
         return output
