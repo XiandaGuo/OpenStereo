20f507d2623ab7aafd295e4757d36eeede0bddc3
chenming.zhang <chenming.zhang>
2023-11-20 12:04:39 +0800

    update yaml

--- scripts/run_fadnet_sceneflow.sh
+++
@@ -1,5 +0,0 @@
-
-#export CUDA_VISIBLE_DEVICES=0,1,2,3
-#python openstereo/main.py --config ./configs/fadnet/FADNet_sceneflow.yaml --scope val 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
-python openstereo/main.py --config ./configs/fadnet/FADNet_sceneflow.yaml --scope train 
