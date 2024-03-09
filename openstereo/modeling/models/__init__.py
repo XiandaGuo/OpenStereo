e8410b0575488122068b3abd2de39203790e20cc
Xianda Guo <xianda_guo@163.com>
2024-03-09 11:33:47 +0800

    Update __init__.py

--- openstereo/modeling/models/__init__.py
+++ openstereo/modeling/models/__init__.py
@@ -1,14 +1,11 @@
-# from .aanet import AANet
-from .acvnet import ACVNet
+# from .aanet import AANet  # 需先编译环境
 from .casnet import CasStereoNet
 from .cfnet import CFNet
-# from .ganet import GANet
 from .coex import CoExNet
 from .fadnet import FADNet
 from .gwcnet import GwcNet
 from .lacgwc import LacGwcNet
 from .msnet import MSNet
 from .psmnet import PSMNet
-from .raftstereo import RAFT_Stereo
 from .sttr import STTRNet
 from .igev import IGEV
